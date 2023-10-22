f252787dafc35d6330b28052964cf66a88f327d5
WombelBat <stefan.bogdan.bodea@gmail.com>
2023-10-22 03:42:37 +0300

    F-Agent Half

--- Stefan/chat.py
+++ Stefan/chat.py
@@ -63,7 +63,7 @@ questions_splitter= CharacterTextSplitter(chunk_size= 1000, chunk_overlap=0,sepa
 questions_chunks = questions_splitter.split_documents(f)
 db = Chroma.from_documents(questions_chunks,OpenAIEmbeddings())
 
-
+print(type(db))
 questions_embeded= OpenAIEmbeddings().embed_query(user_string)
 docs = db.similarity_search_by_vector(questions_embeded)
 
@@ -83,6 +83,29 @@ chain =LLMChain(llm=llm , prompt=query )
 queSys=chain.run(context= context, question = user_string)
 
 print(queSys)
+
+#find category for question this new agent delete if not work
+category_agent =OpenAI()
+category_user_question = user_string
+categories = {"public transport information","pathfinding to a location","anythingh else"}
+
+template_category = "given the following list of categories: {categories} : what category does: {question} : fit best in, tell me only the category?"
+question_category = PromptTemplate(template= template_category, input_variables=["categories","question"])
+chain_category = LLMChain(llm =category_agent , prompt = question_category)
+
+category = chain_category.run(categories=categories, question = category_user_question)
+print(category)
+
+category_temp= Chroma.from_texts(category,OpenAIEmbeddings())
+# db = Chroma.from_documents(questions_chunks,OpenAIEmbeddings())
+temp= "\n".join(categories)
+print(temp)
+
+category_embeded= OpenAIEmbeddings().embed_query(temp)
+category_final = category_temp.similarity_search_by_vector(category_embeded)
+
+print(category_final)
+
 
 
 
