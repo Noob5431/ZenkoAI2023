2b71ab51b51596b5c260c2f644e7e342bee71298
WombelBat <stefan.bogdan.bodea@gmail.com>
2023-10-22 12:12:14 +0300

    final1

---
+++ Stefan/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, jsonify
+
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+from chat import ChatFuction
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    return get_Chat_response(input)
+
+
+def get_Chat_response(text):
+
+    # # Let's chat for 5 lines
+    # for step in range(5):
+    #     # encode the new user input, add the eos_token and return a tensor in Pytorch
+    #     new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+
+    #     # append the new user input tokens to the chat history
+    #     bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+
+    #     # generated a response while limiting the total chat history to 1000 tokens, 
+    #     chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+    #     # pretty print last ouput tokens from bot
+    #     return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    #     # return "https://www.simplilearn.com/ice9/free_resources_article_thumb/Advantages_and_Disadvantages_of_artificial_intelligence.jpg"
+    #     # return "&origin=44.63041983537131, 25.69645319380525,&destination=44.467988484086035, 26.007254223811824"
+    return ChatFuction(text)
+
+if __name__ == '__main__':
+    app.run()
